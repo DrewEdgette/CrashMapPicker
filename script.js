1b287fb0484d964e7db33e95cb6e73ebe5d961ac
DrewEdgette <49006910+DrewEdgette@users.noreply.github.com>
2020-03-23 20:13:03 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,44 +1,63 @@
-
-// map list
-var map_list = ["crash cove", "mystery caves", "sewer speedway", "roo's tubes", "slide coliseum", "turbo track", "coco park", "tiger temple", "papu's pyramid", "dingo canyon", "polar pass", "tiny arena", "dragon mines", "blizzard bluff", "hot air skyway", "cortex castle", "n. gin labs", "oxide station", "inferno island", "jungle boogie", "clockwork wumpa", "android alley", "electron avenue", "deep sea driving", "thunder struck", "tiny temple", "meteor gorge", "barin ruins", "out of time", "assembly lane", "hyper spaceway", "retro stadium", "twilight tour", "prehistoric playground", "spyro circuit", "nina's nightmare", "koala carnival", "gingerbread joyride", "megamix mania"]
-// creates a copy so elements can be deleted
-var copy_list = [];
-
-for (let i = 0; i < map_list.length; i++){
-  copy_list[i] = map_list[i];
+function resetMaps()
+{
+  map_list = [];
+  var scriptUrl = "maps.txt";
+$.ajax({
+  url: scriptUrl,
+  type: 'get',
+  dataType: 'html',
+  async: false,
+  success: function(fileData) 
+  {
+  var line = "";
+  for (var i=0;i<fileData.length;i++)
+  {
+    line += fileData[i];
+    if (fileData[i] == "\n")
+    {
+      map_list.push(line);
+      line = "";
+    }
+  }
+  }
+});
 }
 
-//---------------------------------------------------------------
+
+//--------------------------------------------------------------
 // Code for no-repeat
-//---------------------------------------------------------------
+//--------------------------------------------------------------
 
 
-function getRandomAndDelete(){
+function getRandomAndDelete() {
 
-// if theres more than 1 map in copy_list, display random map name and then delete it from the list.
-if (copy_list.length >= 1) {
-var index = Math.floor(Math.random()*copy_list.length);
-var map = copy_list[index];
-var length = copy_list.length;
+// if theres more than 1 map in map_list, display random map name and then delete it from the list.
+if (map_list.length >= 1) 
+{
+var index = Math.floor(Math.random()*map_list.length);
+var map = map_list[index];
+var length = map_list.length;
 
 
 
 document.getElementById("mapTextArea").style.display = "block";
 
 document.getElementById("mapTextArea").innerHTML = map;
-copy_list.splice(index,1);
+map_list.splice(index,1);
 
 document.getElementById("counterArea").innerHTML = length-1 + " MAPS LEFT";
 
 // if theres only one map left, say so.
-if (copy_list.length == 1) {
+if (map_list.length == 1) 
+{
   document.getElementById("counterArea").innerHTML = "1 MAP LEFT";
 }
 }
 
 
 // otherwise, get rid of map text area and show reset button
-else {
+else 
+{
   document.getElementById("mapTextArea").innerHTML = " ";
   document.getElementById("counterArea").innerHTML =  "NO MAPS LEFT";
 
@@ -49,20 +68,16 @@ else {
   document.getElementById("resetButton").style.display = "block";
 
   document.getElementById("rdmButton").style.display = "none";
-
-
-
 }
 }
 
-function reset(){
 
-// gives all maps back to copy list
-  copy_list = [];
 
-  for (let i = 0; i < map_list.length; i++){
-  copy_list[i] = map_list[i];
-}
+function reset()
+{
+
+// gives all maps back to map list
+  resetMaps();
 
 // resets html
    document.getElementById("rdmButton").style.visibility = "visible";
@@ -88,7 +103,8 @@ function reset(){
 //---------------------------------------------------------------
 
 
-function getRandomMap(){
+function getRandomMap()
+{
   var index = Math.floor(Math.random()*map_list.length);
   var map = map_list[index];
 
@@ -96,3 +112,5 @@ function getRandomMap(){
   document.getElementById("mapTextArea").style.display = "block";
 }
 
+// resets the map list when the page is loaded up
+resetMaps();
